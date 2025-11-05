4155025f030172302ac97675184465c647e2b7e1
Danny AS <dannyalfaros45@gmail.com>
2025-11-05 17:26:16 -0600

    rename file txt generated

--- src/logic/graph_representations.py
+++ src/logic/graph_representations.py
@@ -1,5 +1,4 @@
 import networkx as nx
-import osmnx as ox
 
 
 def write_to_file(file_name, line, mode = "a"):
@@ -7,7 +6,7 @@ def write_to_file(file_name, line, mode = "a"):
         file.write(line + "\n")
 
 
-def save_adjacency_list(graph, file_name: str = "adyacent_list.txt"):
+def save_adjacency_list(graph, file_name: str = "adjacency_list.txt"):
     write_to_file(file_name, "", mode="w")
     mapping = {node: i for i, node in enumerate(graph.nodes())}
     G = nx.relabel_nodes(graph, mapping)
